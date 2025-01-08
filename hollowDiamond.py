26c6ae4f3cfd7f598a017aaeb8a44cec1db58425
chandhan <chandhan.g0401@gmail.com>
2025-01-08 23:53:05 +0530

    edit

--- hollowDiamond.py
+++ hollowDiamond.py
@@ -10,11 +10,11 @@ for i in range(r-1):
         else:
             print(" ",end=" ")
     print()
-for i in range(r-2,-1,-1):
+for i in range(r-1,-1,-1):
     for j in range(r-i-1):
         print(" ",end=" ")
     for k in range(i*2+1):
-        if(i==r-1 or k==0 or k==i*2 ):
+        if(  k==0 or k==i*2 ):
             
             print("*",end=" ")
         else:
