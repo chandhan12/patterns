c322ded3f118d9c46729ee780e32f1f68d70f7c8
chandhan <chandhan.g0401@gmail.com>
2025-01-08 22:39:48 +0530

    hollow triangle,diamond

---
+++ ReverseHollowTriangle.py
@@ -0,0 +1,11 @@
+r=6
+for i in range(r-1,-1,-1):
+    for j in range(r-i-1):
+        print(" ",end=" ")
+    for k in range(i*2+1):
+        if(i==r-1 or k==0 or k==i*2 ):
+            
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
